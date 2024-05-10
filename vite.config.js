bca109c482821df80f2c142cf55fdae47bbc3991
Aung Myint Than <blueice085@gmail.com>
2024-05-10 23:00:06 +0630

    Vite config for multi-page updated

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   //base: "/NovaGoal/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        jp: resolve(__dirname, "jp.html"),
+        //nested: resolve(__dirname, "jp/index.html"),
+      },
+    },
+  },
 });
